5b42146ed8ea7d031eb01a721c2e61262944afa2
d19960217 <fengdai977@gmail.com>
2023-10-11 15:55:53 +0800

    move addtocart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart=[];
+
+export function addToCart(productId){
+  let matchingItem;
+   cart.forEach((cartItem)=>{
+    if(productId===cartItem.productId){
+     matchingItem =cartItem;
+    }
+   });
+   //判断相同按钮后加1
+   if(matchingItem){
+    matchingItem.quantity+=1;
+   }else{
+    cart.push(
+      {productId:productId,
+        quantity: 1
+      }
+    );
+   }
+}
